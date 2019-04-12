adb5a4fe11be958dcca9098920e1fed68dec1d9c
Gareth Rees <gareth@garethrees.co.uk>
2019-04-12 13:09:09 +0100

    Calculate the scores and render the radar chart
    Calculates the 1-5 score of each area and renders them in to the radar
    chart.

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,67 @@ var submitQuestionForm = function submitQuestionForm(evt) {
   }
 }
 
+// Get all answer key/value pairs starting with the given prefix
+//
+// Example:
+//
+//     getAnswerGroup('people');
+var getAnswerGroup = function getAnswerGroup(prefix) {
+  let pattern = "^" + prefix + "-";
+  let regexp = new RegExp(pattern);
+
+  return Object.keys(sessionStorage)
+           .filter(key => regexp.test(key))
+           .reduce((obj, k) => {
+             return { ...obj, [k]: sessionStorage.getItem(k)}}, {});
+}
+
+// Calculate 1-5 score for a given prefix
+//
+// Example:
+//
+//     calculateScore('people');
+var calculateScore = function calculateScore(prefix) {
+  let group = getAnswerGroup(prefix);
+
+  let minScore = 0;
+  let maxScore = Object.keys(group).length * 3;
+  let totalScore = Object.values(group).reduce(
+    function(t, s) { return parseInt(t) + parseInt(s); }
+  );
+  let percentage = (totalScore / maxScore) * 100;
+  let finalScore = (percentage * 5) / 100;
+
+  return Math.round(finalScore);
+}
+
+// Calculate 1-5 score for all question groups
+var calculateScores = function calculateScores() {
+  return {
+    people: calculateScore('people'),
+    process: calculateScore('process'),
+    technology: calculateScore('technology')
+  }
+}
+
+var buildRadarChartData = function buildRadarChartData(scores) {
+  return [
+    {
+      axes: [
+        { axis: "people", value: scores.people },
+        { axis: "process", value: scores.process },
+        { axis: "technology", value: scores.technology }
+      ],
+    }
+  ];
+}
+
+var renderRadarChart = function renderRadarChart() {
+  let scores = calculateScores();
+  var data = buildRadarChartData(scores);
+  RadarChart.draw(".results-radar-chart", data);
+}
+
 var renderAnswers = function renderAnswers() {
   $('[data-question]').each(function() {
     let key = this.dataset.question;
@@ -35,20 +96,8 @@ var configureRadarChartDefaults = function configureRadarChartDefaults() {
 
 $(function(){
   configureRadarChartDefaults();
-
-  var data = [
-    {
-      axes: [
-        { axis: "people", value: 2 },
-        { axis: "process", value: 2 },
-        { axis: "technology", value: 4 }
-      ],
-    }
-  ];
-
-  RadarChart.draw(".results-radar-chart", data);
-
   $('.question-form').submit(submitQuestionForm);
+  $(".results-page").ready(renderRadarChart);
   $(".results-page").ready(renderAnswers);
   $(".retake-test").click(clearAnswers);
 });
